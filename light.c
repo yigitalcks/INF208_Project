1459d8837cd9bcaeb77dc7032da6e6eb387a27fe
Berkay <60315799+whofeeddrogon@users.noreply.github.com>
2023-05-18 02:32:31 +0300

    sence tek satırlık if ve while statementlardaki bracetleri silmek havalı mı

--- light.c
+++ light.c
@@ -8,49 +8,49 @@
 #define Y_PIN   24  // GPIO 19 | Yellow Led pin
 #define G_PIN   25  // GPIO 26 | Green Led pin
 	
-uint32_t lightLevel(int pin) {
+uint32_t lightLevel (int pin) {
 	uint32_t reading = 0;
-	pinMode(LDR_PIN, OUTPUT);
-	digitalWrite(LDR_PIN, LOW);
-	delay(1000);
+	pinMode (LDR_PIN, OUTPUT);
+	digitalWrite (LDR_PIN, LOW);
+	delay (1000);
 	
-	pinMode(LDR_PIN, INPUT);
+	pinMode (LDR_PIN, INPUT);
 	
-	while(digitalRead(LDR_PIN) == LOW) {
+	while (digitalRead (LDR_PIN) == LOW) {
 		reading++;
 	}
     return reading;
 }
 
-int main(void) {
+int main (void) {
 
-    if(wiringPiSetup() == -1) {
-        exit(1);
+    if (wiringPiSetup () == -1) {
+        exit (1);
     }
 
-    pinMode(R_PIN, OUTPUT);
-    pinMode(Y_PIN, OUTPUT);
-    pinMode(G_PIN, OUTPUT);
+    pinMode (R_PIN, OUTPUT);
+    pinMode (Y_PIN, OUTPUT);
+    pinMode (G_PIN, OUTPUT);
 
     uint32_t reading;
-    while(TRUE) {
+    while (TRUE) {
         reading = lightLevel(LDR_PIN);
-        if(reading < 1000) {
-            digitalWrite(G_PIN, HIGH);
-            digitalWrite(R_PIN, LOW);
-            digitalWrite(Y_PIN, LOW);
+        if (reading < 1000) {
+            digitalWrite (G_PIN, HIGH);
+            digitalWrite (R_PIN, LOW);
+            digitalWrite (Y_PIN, LOW);
         }
         else if (reading > 8000) {
-            digitalWrite(G_PIN, LOW);
-            digitalWrite(R_PIN, LOW);
-            digitalWrite(Y_PIN, HIGH);
+            digitalWrite (G_PIN, LOW);
+            digitalWrite (R_PIN, LOW);
+            digitalWrite (Y_PIN, HIGH);
         }
         else {
-            digitalWrite(G_PIN, LOW);
-            digitalWrite(R_PIN, HIGH);
-            digitalWrite(Y_PIN, LOW);
+            digitalWrite (G_PIN, LOW);
+            digitalWrite (R_PIN, HIGH);
+            digitalWrite (Y_PIN, LOW);
         }
-        printf("%u\n", reading);
+        printf ("%u\n", reading);
     }
     return 0;
 }
